2260bf83a77efbd2e9cf96958f20ac2d1ec4c857
Lerxuanyikate <kateler6517@gmail.com>
2026-01-06 11:53:20 +0800

    v

--- server.js
+++ server.js
@@ -38,3 +38,16 @@ app.get('/allcards', async (req, res) => {
         res.status(500).json({ error: 'Server Error for allcards' });
     }
 });
+
+//example routeL add card
+app.post('/addcard', async (req, res) => {
+    const { name, type, rarity } = req.body;    
+    try {
+        let connection = await mysql.createConnection(dbConfig);    
+        await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?, ?)', [card_name, card_pic]);    
+        res.status(201).json({ message: 'Card '+card_name+' added successfully'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server Error - Could not add card'+card_name });
+    }       
+});
